6bd40b3eae039be9f04b167198ab516bc08ac57f
diahmustikamundingwangi-max <diahmustikamundingwangi@gmail.com>
2026-04-02 10:46:07 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,7 @@
+x = int(input("Masukkan jumlah hari: "))
+tahun = x // 365
+sisa_hari = x % 365
+bulan = sisa_hari // 30
+hari = sisa_hari % 30
+
+print(f"{tahun} tahun, {bulan} bulan, {hari} hari")
